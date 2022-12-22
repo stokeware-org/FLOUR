755a600872148388a50f894b6dd2dcdece0e1dcb
Lukas Turcani <lukasturcani@mailbox.org>
2022-12-22 22:22:16 +0000

    Add type hints

--- flour.pyi
+++ flour.pyi
@@ -1,7 +1,31 @@
 import pathlib
 
+import numpy as np
+import numpy.typing as npt
+
+class VoxelGrid:
+    voxels: npt.NDArray[np.float64]
+    origin: npt.NDArray[np.float64]
+    voxel_size: npt.NDArray[np.float64]
+
 class CubeData:
-    atoms: list[int]
+    atoms: npt.NDArray[np.uint8]
+    charges: npt.NDArray[np.float64]
+    positions: npt.NDArray[np.float64]
+    grid: VoxelGrid
 
 def read_cube(path: pathlib.Path | str) -> CubeData:
+    pass
+
+def write_cube(
+    path: pathlib.Path | str,
+    title1: str,
+    title2: str,
+    atoms: npt.NDArray[np.uint8],
+    charges: npt.NDArray[np.float64],
+    positions: npt.NDArray[np.float64],
+    voxel_origin: npt.NDArray[np.float64],
+    voxel_size: npt.NDArray[np.float64],
+    voxels: npt.NDArray[np.float64],
+) -> None:
     pass
